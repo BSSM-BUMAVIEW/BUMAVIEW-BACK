605ba03e727448ca85a1a1d8cf13df04ae2defe9
허세진 <heosejin222@gmail.com>
2025-09-12 01:50:42 +0900

    Initial commit

---
+++ src/test/java/com/bssm/bumaview/BumaviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bssm.bumaview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumaviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
